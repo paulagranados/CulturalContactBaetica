deffd9f8a2ebd91f1c808fe34733b906be64fc0d
Alessandro Adamou <alexdma@apache.org>
2018-01-23 13:48:37 +0000

    Added setup script
    Changes to be committed:
    	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Cultural Contact Baetica',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Paula Granados Garcia',
+      author_email='',
+      url='https://github.com/paulagranados/CulturalContactBaetica',
+      install_requires=[
+         'rdflib'
+      ],
+)
