e908d96da38ccc5d8fbeca1d36868e1beab30abf
Alessandro Adamou <alexdma@apache.org>
2018-01-31 16:29:32 +0000

    Nomisma now runs

--- src/nomisma_sheet.py
+++ src/nomisma_sheet.py
@@ -1,11 +1,12 @@
-import google 
+import google
 
-import rdfLib
-from rdfLib import Graph, Namespace, URIRef
+import rdflib
+from rdflib import Graph, Namespace, URIRef
 import re
 from urllib.parse import urlparse, parse_qs
 
-#Define Utility RDF prefixes 
+# Define Utility RDF prefixes
+crm = Namespace('http://www.cidoc-crm.org/cidoc-crm/')
 skos = Namespace ('http://www.w3.org/2004/02/skos/core#') 
 prov = Namespace ('http://www.w3.org/ns/prov#') 
 foaf = Namespace ('http://xmlns.com/foaf/0.1/') 
@@ -21,40 +22,15 @@ rdfs = Namespace ('http://www.w3.org/2000/01/rdf-schema#')
 un = Namespace ('http://www.owl-ontologies.com/Ontology1181490123.owl#') 
 
 vocabs = {
-    'material': 'http://nomisma.org/ontology#Material' 
-    'type': 'http://nomisma.org/ontology#ObjectType'
-    'mint': 'http://nomisma.org/ontology#Mint'     
-    'date': 'https://www.eagle-network.eu/voc/dates/">' 
-    'endDate': 'http://nomisma.org/ontology#EndDate'  
-    'legend': 'http://nomisma.org/ontology#Legend'
-    'iconography': 'http://nomisma.org/ontology#Iconography'
-    'findspot': 'http://nomisma.org/ontology#Findspot'
-    'issuer': 'http://nomisma.org/ontology#Issuer' 
-    'ethnic': 'http://nomisma.org/ontology#Ethnic'
-    
+    'dates': 'http://www.eagle-network.eu/voc/dates.rdf', 
+    'nomisma': 'http://nomisma.org/ontology.rdf'
 }
 
 # Load the Nomisma vocabularies so we can query them locally
-g_material = Graph()
-g_material.parse(vocabs['material'], format="xml")
-g_object_type = Graph()
-g_object_type.parse(vocabs['object_type'], format="xml")
-g_mint = Graph ()
-g_mint.parse(vocabs['mint'], format="xml")
-g_date = Graph()
-g_date.parse(vocabs['date'], format="xml")
-g_end_date = Graph()
-g_end_date.parse(vocabs['end_date'], format="xml")
-g_legend = Graph()
-g_legend.parse(vocabs['legend'], format="xml")
-g_iconography = Graph()
-g_iconography.parse(vocabs['iconography'], format="xml")
-g_findspot = Graph()
-g_fndspot.parse(vocabs['findspot'], format="xml")
-g_issuer = Graph()
-g_issuer.parse(vocabs['issuer'], format="xml")
-g_ethnic = Graph()
-g_ethnic.parse(vocabs['ethnic'], format="xml")
+g_dates = Graph()
+g_dates.parse(vocabs['dates'], format="xml")
+g_nomisma = Graph()
+g_nomisma.parse(vocabs['nomisma'], format="xml")
 
 def lookup_nomisma(label, gr, material, object_type, mint, date, end_date, legend, iconography, findspot, issue, ethnic):
 	"""Tries to find a string match for a given label in a selected
