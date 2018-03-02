c7316dc28621f609b7f571beb3d730a8ee6beaff
paulagranados <paulagranadosgarcia@gmail.com>
2018-03-02 12:15:42 +0000

    Update sparql_exercise1.py

--- Exercises/sparql_exercise1.py
+++ Exercises/sparql_exercise1.py
@@ -11,6 +11,7 @@ sparql.setQuery(
   ?y rso:PX_object_type/skos:broader* thes:x6089
   ; rso:Thing_from_Place/(<http://www.cidoc-crm.org/cidoc-crm/P88i_forms_part_of>|^rso:Place_has_part_Place|skos:broader)* <http://collection.britishmuseum.org/id/place/x22782>
 }
+)
 Sparql.setReturnFormat(RDF)
 results = sparql.query().convert()
 for results in results: print (result).  
