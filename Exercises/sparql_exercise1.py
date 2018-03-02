bac2dfa24817ac1578ccd7bca480dcbdc08bb694
paulagranados <paulagranadosgarcia@gmail.com>
2018-03-02 12:15:13 +0000

    Update sparql_exercise1.py

--- Exercises/sparql_exercise1.py
+++ Exercises/sparql_exercise1.py
@@ -2,16 +2,15 @@ import rdfLib
 from rdfLib import Graph, Namespace, UriRef
 pip install SPARQLWrapper
 from SPARQLWrapper import SPARQLWrapper
-sparql = SPARQLWrapper ("https://collection.britishmuseum.org/sparql")
-sparql.setQuery
-#Finds all the coins from the province of Andalucia
-     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-     PREFIX thes: <http://collection.britishmuseum.org/id/thesauri/>
-     PREFIX rso: <http://www.researchspace.org/ontology/>
+sparql = SPARQLWrapper("https://collection.britishmuseum.org/sparql") 
+sparql.setQuery(
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX thes: <http://collection.britishmuseum.org/id/thesauri/>
+    PREFIX rso: <http://www.researchspace.org/ontology/>
      SELECT DISTINCT ?y WHERE {
   ?y rso:PX_object_type/skos:broader* thes:x6089
   ; rso:Thing_from_Place/(<http://www.cidoc-crm.org/cidoc-crm/P88i_forms_part_of>|^rso:Place_has_part_Place|skos:broader)* <http://collection.britishmuseum.org/id/place/x22782>
 }
 Sparql.setReturnFormat(RDF)
 results = sparql.query().convert()
-for results in results: print (result). 
+for results in results: print (result).  
