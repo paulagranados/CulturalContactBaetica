4fa017c2050bdfc0803960d99f1cf0ab63c99c1b
PAULA  GRANADOS GARCIA <paulagranadosgarcia@MacBook-Air-de-PAULA.local>
2018-03-08 18:37:42 +0000

    sparql corrections

--- Exercises/sparql_exercise1.py
+++ Exercises/sparql_exercise1.py
@@ -1,17 +1,19 @@
-import rdfLib
-from rdfLib import Graph, Namespace, UriRef
-pip install SPARQLWrapper
-from SPARQLWrapper import SPARQLWrapper
+#pip install SPARQLWrapper
+#python
+
+from SPARQLWrapper import Wrapper, RDF, TURTLE
 sparql = SPARQLWrapper("https://collection.britishmuseum.org/sparql") 
-sparql.setQuery(
+sparql.setQuery("""
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
     PREFIX thes: <http://collection.britishmuseum.org/id/thesauri/>
-    PREFIX rso: <http://www.researchspace.org/ontology/>
-     SELECT DISTINCT ?y WHERE {
+    PREFIX rso: <http://www.researchspace.org/ontology/>PREFI
+     CONSTRUCT DISTINCT ?y WHERE {
   ?y rso:PX_object_type/skos:broader* thes:x6089
   ; rso:Thing_from_Place/(<http://www.cidoc-crm.org/cidoc-crm/P88i_forms_part_of>|^rso:Place_has_part_Place|skos:broader)* <http://collection.britishmuseum.org/id/place/x22782>
 }
-)
-Sparql.setReturnFormat(RDF)
+""")
+sparql.setReturnFormat(TURTLE)
 results = sparql.query().convert()
-for results in results: print (result).  
+for result in results:   
+    print(results.serialize)
+    
