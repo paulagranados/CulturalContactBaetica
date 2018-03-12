0af688a935c1ce4dcc59701baaf2bb986bab6ee5
paulagranados <paulagranadosgarcia@gmail.com>
2018-03-12 11:04:22 +0000

    Update sparql_exercise1.py

--- Exercises/sparql_exercise1.py
+++ Exercises/sparql_exercise1.py
@@ -8,7 +8,7 @@ sparql.setQuery("""
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
     PREFIX thes: <http://collection.britishmuseum.org/id/thesauri/>
     PREFIX rso: <http://www.researchspace.org/ontology/>
-     CONSTRUCT DISTINCT ?y WHERE {
+     SELECT DISTINCT ?y WHERE {
   ?y rso:PX_object_type/skos:broader* thes:x6089
   ; rso:Thing_from_Place/(<http://www.cidoc-crm.org/cidoc-crm/P88i_forms_part_of>|^rso:Place_has_part_Place|skos:broader)* <http://collection.britishmuseum.org/id/place/x22782>
 }
