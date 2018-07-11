3e5e8a1d5d9a1a638f6871ab00055634742df835
PAULA  GRANADOS GARCIA <paulagranadosgarcia@MacBook-Air-de-PAULA.local>
2018-07-11 17:01:43 +0100

    moved files to another directory

--- Exercises/sparql_exercise1.py
+++ Exercises/sparql_exercise1.py
@@ -8,23 +8,12 @@ sparql.setQuery("""
     PREFIX skos: <http://www.w3.org/2004/02/skos/core>
     PREFIX thes: <http://collection.britishmuseum.org/id/thesauri/>
     PREFIX rso: <http://www.researchspace.org/ontology/>
-     SELECT DISTINCT ?y WHERE {
-  ?y rso:PX_object_type/skos:broader* thes:x6089
+     SELECT DISTINCT ?coin WHERE {
+  ?coin rso:PX_object_type/skos:broader* thes:x6089
   ; rso:Thing_from_Place/(<http://www.cidoc-crm.org/cidoc-crm/P88i_forms_part_of>|^rso:Place_has_part_Place|skos:broader)* <http://collection.britishmuseum.org/id/place/x22782>
-<<<<<<< HEAD
-} LIMIT 10  
-=======
-}  LIMIT 10
->>>>>>> b9b660fe6015b223c72b7d871b6ac435e0d1cea0
+  }  LIMIT 10
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
-for result in results:   
-    print(results.serialize)
-    
-<<<<<<< HEAD
-    
-    
-=======
-   
->>>>>>> b9b660fe6015b223c72b7d871b6ac435e0d1cea0
+for result in results ["results"]["bindings"]:
+    print (result["coin"]["value"])
