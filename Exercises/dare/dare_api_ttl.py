2e04f3bc1545a3d3cdf7d6ad771daf24a7475d4a
Paula Granados <paulagranadosgarcia@gmail.com>
2018-07-17 16:42:23 +0100

    changes to parse call

--- Exercises/dare/dare_api_ttl.py
+++ Exercises/dare/dare_api_ttl.py
@@ -16,5 +16,32 @@ with open('dare-test.txt') as baetica:
 #                data = url.read().decode(http_info.get_content_charset())
                 data = url.read().decode('utf8')
                 dare_places_in_baetica = Graph()
-                dare_places_in_baetica.parse(data,format=rdflib.util.guess_format())
-    
+                dare_places_in_baetica.parse(data=data,format=rdflib.util.guess_format(pl))
+ 				print (data) 
+ 				               
+#from rdflib import Graph
+#import re
+#import urllib.request, urllib.parse, urllib.error
+#from urllib.request import urlopen
+#import pprint
+
+
+#with open('dare-test.txt') as baetica:
+ #   b = []
+  #  for l in baetica:
+   #     if re.match('http://',l):
+    #        pl = l.rstrip()+'.ttl'
+     #       print(pl)
+      #      g = Graph()
+       #     bae = g.parse(pl)
+        #    print(bae)
+            
+            
+#            with urlopen(pl) as url:
+#                http_info = url.info()
+#                print(http_info)
+#                data = url.read().decode(http_info.get_content_charset())
+#                ttl = url.read().decode('utf8')
+#                g = Graph()
+#                g.parse(data=url.read().decode('utf8'),format="n3")
+#                print(len(g))
