1a1ca849d9139a254f7718b84f11ffe4b489ee72
alexdma <alexdma@apache.org>
2018-07-25 16:15:56 +0100

    Nomisma supports material and authority

--- src/commons/rdf.py
+++ src/commons/rdf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from rdflib import Graph, Literal, Namespace, OWL, RDF, RDFS, URIRef
+from rdflib.namespace import FOAF
 import unidecode
 
 crm = Namespace('http://erlangen-crm.org/current/')
@@ -10,10 +11,12 @@ def make_basic_entity(label, graph, type):
 	if not label : return None
 	base_uri = 'http://data.open.ac.uk/baetica/'
 	lsani = unidecode.unidecode(label.strip().lower().replace(' ','_'))
-	if   type ==       crm.E55_Type : t = 'objtyp'
+	if   type ==               None : t = 'thing'
+	elif type ==       crm.E55_Type : t = 'objtyp'
 	elif type ==   crm.E57_Material : t = 'material'
 	elif type == crm.E78_Collection : t = 'collection'
-	else: t = 'thing'
+	elif type ==         FOAF.Agent : t = 'agent'
+	else : t = type.strip().lower().replace(' ','_')
 	subj = URIRef(base_uri + t + '/' + lsani)
 	graph.add( ( subj, RDF.type, URIRef(type) ) )
 	graph.add( ( subj, RDFS.label, Literal(label,lang='en') ) )
